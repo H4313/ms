4e33e05cf28b1134af122dab49d2a2adacb2db24
somarariana <somarariana@yahoo.com>
2014-02-08 16:15:12 +0100

    test room method

--- mSensor/src/com/h4313/deephouse/msensor/main/Main.java
+++ mSensor/src/com/h4313/deephouse/msensor/main/Main.java
@@ -33,7 +33,7 @@ public class Main
 				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.PRESENCE);
 
 				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.LIGHTCONTROL);
-				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.AIRCONDITION);
+				//room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.AIRCONDITION);
 				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.RADIATOR);
 				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.WINDOWCLOSER);
 			}
