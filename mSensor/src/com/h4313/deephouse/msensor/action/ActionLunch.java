5bc2717a9016a74fbbddec2e3d1cfd4e0975e877
macbric <macbric>
2014-02-18 13:46:47 +0100

    Ajout action de déjeuner

---
+++ mSensor/src/com/h4313/deephouse/msensor/action/ActionLunch.java
@@ -0,0 +1,41 @@
+package com.h4313.deephouse.msensor.action;
+
+import com.h4313.deephouse.housemodel.House;
+import com.h4313.deephouse.housemodel.RoomConstants;
+import com.h4313.deephouse.sensor.SensorType;
+
+public class ActionLunch
+{
+	private static volatile ActionLunch instance = null;
+	
+	private ActionLunch()
+	{
+	}
+	
+	/**
+     * Méthode permettant de renvoyer une instance de la classe Singleton
+     * @return Retourne l'instance du singleton.
+     */
+    public final static ActionLunch getInstance() {
+        if (ActionLunch.instance == null) {
+           synchronized(ActionLunch.class) {
+             if (ActionLunch.instance == null) {
+            	 ActionLunch.instance = new ActionLunch();
+             }
+           }
+        }
+        return ActionLunch.instance;
+    }
+	
+	public void run()
+	{
+		House.getInstance().updateSensor(SensorType.PRESENCE, (Boolean)false);
+		House.getInstance().updateSensor(SensorType.LIGHT, (Boolean)false);
+		House.getInstance().updateSensor(RoomConstants.ID_CORRIDOR, SensorType.PRESENCE, (Boolean)true);
+		House.getInstance().updateSensor(RoomConstants.ID_CORRIDOR, SensorType.LIGHT, (Boolean)true);
+		House.getInstance().updateSensor(RoomConstants.ID_LIVING_ROOM, SensorType.PRESENCE, (Boolean)true);
+		House.getInstance().updateSensor(RoomConstants.ID_LIVING_ROOM, SensorType.LIGHT, (Boolean)true);
+		House.getInstance().updateSensor(RoomConstants.ID_KITCHEN, SensorType.PRESENCE, (Boolean)true);
+		House.getInstance().updateSensor(RoomConstants.ID_KITCHEN, SensorType.LIGHT, (Boolean)true);
+	}
+}
